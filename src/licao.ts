b3a330b57f555888e5c0b0db9ccf2e92b759f2e1
Wanderx13 <pupetfuntasy43@gmail.com>
2025-01-07 18:08:24 -0300

    first commit

---
+++ src/licao.ts
@@ -0,0 +1,13 @@
+function multiplicacao(n1:3, n2:1): number {
+    return n1 * n2
+}
+
+function saudacao(nome:'Julia'): string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicacao(3,1))
+console.log(saudacao("Julia"))
+
+
+
